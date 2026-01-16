4711bb116ab2aa63002b4a32b791d2459aadb13a
jo-anb <j.n.balker@outlook.com>
2026-01-16 11:26:32 +0100

    build: disable schedule

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 export const INTEGRATION="simple_plant_extended";
 export const CARD_TYPE="simple-plant-extended-card";
 export const CARD_NAME="Simple Plant Extended Card";
-export const CARD_DESCRIPTION="Custom card for simple-plant extended integration";
+export const CARD_DESCRIPTION="Custom card for simple-plant-extended integration";
 
 export const CARD_VERSION="v1.0.0";
 export const CARD_AUTHOR="jo-anb";
